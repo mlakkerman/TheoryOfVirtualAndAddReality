96352da7bac21e9ae406dcbfc4230e5b09615116
mlakkerman <nonamesyntax@mail.ru>
2024-03-20 19:48:27 +0400

    second laboratory

--- src/init.js
+++ src/init.js
@@ -1,9 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// сцена состоит из: камеры, освещения, геом. моделей
 const init = () => {
-	//размер окна
+	//Размер окна
 	const sizes = {
 		width: window.innerWidth,
 		height: window.innerHeight,
@@ -11,12 +10,14 @@ const init = () => {
 	//Создание сцены, отрисовка в canvas
 	const scene = new THREE.Scene();
 	const canvas = document.querySelector('.canvas');
+	//создание камеры и добавление на сцену
 	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 	scene.add(camera);
-
+	
+	//6. Подключить библиотеку OrbitControls и обеспечить с помощью нее управление камерой в сцене.
 	const controls = new OrbitControls(camera, canvas);
 	controls.enableDamping = true;
-
+	//рендер, рисующий сцену
 	const renderer = new THREE.WebGLRenderer({ canvas });
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.shadowMap.enabled = true;
