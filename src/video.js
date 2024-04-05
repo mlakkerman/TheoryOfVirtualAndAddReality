6f40c7362fc92ef68bf13c3ebe347bda854c3d1b
mlakkerman <nonamesyntax@mail.ru>
2024-04-05 11:38:45 +0400

    begin LW 3

--- src/video.js
+++ src/video.js
@@ -1,32 +1,4 @@
 import './style.css';
-import Hammer from 'hammerjs';
-import init from './threeScene';
-
-const { sizes, camera, scene, canvas, controls, renderer } = init();
-
-// Инициализация Hammer
-function initHammerGestures() {
-    const videoElement = document.getElementById('video');
-    const hammer = new Hammer(videoElement);
-
-    const panHandler = (event) => {
-        const speedX = 0.002; // настраиваемая скорость панорамного движения по оси X
-        const speedY = 0.002; // настраиваемая скорость панорамного движения по оси Y
-        controls.pan(event.deltaX * speedX, event.deltaY * speedY);
-    };
-
-    const pinchHandler = (event) => {
-        const dollySpeed = 0.05;
-        if (event.additionalEvent === "pinchin") {
-            controls.dollyIn(1 + dollySpeed * event.scale);
-        } else if (event.additionalEvent === "pinchout") {
-            controls.dollyOut(1 + dollySpeed * event.scale);
-        }
-    };
-
-    hammer.on('panmove', panHandler);
-    hammer.on('pinch', pinchHandler);
-}
 
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
     navigator.mediaDevices.getUserMedia({ video: true })
@@ -34,8 +6,6 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
             const video = document.getElementById('video');
             video.srcObject = stream;
             video.play();
-            console.log(video);
-            initHammerGestures();
         })
         .catch(function (error) {
             console.error("Ошибка при доступе к камере", error);
